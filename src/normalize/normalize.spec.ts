419b5261e44eb34410503ed13ece658d91ba68d9
Brian Butz <butzopower@gmail.com>
2022-01-09 17:37:12 +0000

    Implement normalize

--- src/normalize/normalize.spec.ts
+++ src/normalize/normalize.spec.ts
@@ -15,4 +15,24 @@ describe('normalize', () => {
 
     expect(normalized).to.eql(alreadyNormalized);
   });
+
+  it('converts leading underscores to capitalize the next letter', () => {
+    const toBeNormalized = {
+      id: 1,
+      parent_id: 2,
+      first_name: 'Max',
+      last_name: 'Mustermann'
+    };
+
+    const normalized = normalize(toBeNormalized);
+
+    expect(normalized).to.eql(
+      {
+        id: 1,
+        parentId: 2,
+        firstName: 'Max',
+        lastName: 'Mustermann',
+      }
+    );
+  });
 });
--- src/normalize/normalize.ts
+++ src/normalize/normalize.ts
@@ -1,5 +1,21 @@
 import { type Normalized } from './types';
 
-export function normalize<T>(alreadyNormalized: T): Normalized<T> {
-  return alreadyNormalized;
+function upcaseFirst(s: string): string {
+  return `${s[0].toUpperCase()}${s.slice(1)}`;
+}
+
+function normalizeString(key: string): string {
+  return key.split("_")
+    .map((part, index) => index === 0 ? part : upcaseFirst(part))
+    .join('');
+}
+
+export function normalize<T extends object>(toBeNormalized: T): Normalized<T>
+export function normalize(toBeNormalized: object): object {
+  const normalized = Object.entries(toBeNormalized)
+    .reduce((acc, [key, value]) => {
+      return Object.assign(acc, {[normalizeString(key)]: value});
+    }, {} as object);
+
+  return normalized;
 }
