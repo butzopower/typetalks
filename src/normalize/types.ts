c4d6bde59d8d732af4f8a1d360b16b69f693f410
Brian Butz <butzopower@gmail.com>
2022-01-09 17:41:29 +0000

    normalize/Normalized type

--- src/normalize/types.ts
+++ src/normalize/types.ts
@@ -1,1 +1,16 @@
-export type Normalized<T> = T;
+type normalize<s extends string> =
+  s extends `${infer start}_${infer next}${infer rest}` ?
+    normalize<`${start}${Uppercase<next>}${rest}`> :
+    s;
+
+export type Normalized<T extends object> = {
+  [Property in keyof T as normalize<string & Property>]: T[Property]
+}
+
+// tests
+
+const stringType: normalize<string> = '' as string;
+const alreadyNormal: normalize<"normal"> = "normal";
+const singleUnderscore: normalize<"not_normal"> = "notNormal";
+const doubleUnderscore: normalize<"more_not_normal"> = "moreNotNormal";
+const sentenceUnderscore: normalize<"four_score_and_twenty_years_ago"> = "fourScoreAndTwentyYearsAgo";
